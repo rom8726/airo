39e8d57ff441860c37ab8ec70a0bea5fb53432b1
Roman Chudov <roman.chudov@gmail.com>
2025-05-11 16:29:12 +0300

    Log messages added.

--- generator/generator.go
+++ generator/generator.go
@@ -9,20 +9,26 @@ import (
 )
 
 func GenerateProject(ctx context.Context, cfg *config.ProjectConfig) error {
+	fmt.Println("Generate project directory...")
 	root := cfg.ProjectName
 	if err := os.MkdirAll(root, 0755); err != nil {
 		return err
 	}
+	fmt.Println("project directory: done")
 
+	fmt.Println("Write go.mod...")
 	// 1. go.mod
 	if err := writeGoMod(cfg); err != nil {
 		return err
 	}
+	fmt.Println("go.mod: done")
 
+	fmt.Println("Generate OpenAPI server with ogen...")
 	// 2. ogen
 	if err := runOGen(cfg); err != nil {
 		return fmt.Errorf("ogen failed: %w", err)
 	}
+	fmt.Println("ogen: done")
 
 	//// 3. main.go, app.go, config.go
 	//if err := writeMain(cfg); err != nil {
